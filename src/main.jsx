841ca7e880fac30572d1db87bc346fe59dd63cb4
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-02-12 20:02:18 +0100

    start files Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { Toaster } from "react-hot-toast";
-import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      <Toaster position="top-right" reverseOrder={false} />
-    </>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
