b135c22cd6a1cd01f7e48e110f9f95c5bbb80480
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-02-15 18:59:31 +0100

    redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./counter/counterSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "counter-persist",
+  version: 1,
+  storage,
+  blacklist: ["step"],
+  whitelist: ["counter"],
+};
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: persistReducer(persistConfig, counterReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
